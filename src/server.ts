7420a76646c22eff8a29307761f9c6f88817f4e9
Gustavo Rigoni <gustrigoni@gmail.com>
2021-09-16 11:27:41 -0300

    added web sockets in the server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-import { app } from "./app";
-import { router } from "./routes";
+import { Server } from "socket.io";
+import { app } from "./api/app";
+import { router } from "./api/routes";
+import http from 'http';
 
+const port = process.env.API_PORT;
 
-app.listen(3333, () => console.log('Listening on port: 3333'));
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(port, () => console.log(`Listening REST API on port: ${port}`));
 app.use(router);
